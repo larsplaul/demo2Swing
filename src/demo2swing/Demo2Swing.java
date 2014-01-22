5e0ef6def6381d0521be1579fa854afa60c15690
Bruger 1 <larsesspam@gmail.com>
2014-01-22 09:26:55 +0100

    Initial Commit

---
+++ src/demo2swing/Demo2Swing.java
@@ -0,0 +1,19 @@
+/*
+ */
+
+package demo2swing;
+
+/**
+ *
+ * @author Lars Mortensen (lam@cphbusiness.dk)
+ */
+public class Demo2Swing {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
